b0830bac5e7d3911494a1cf87fe7ff5d9597f145
liguang <940102569@qq.com>
2020-10-02 22:15:50 +0800

    aa

--- src/cn/itcast/code/day12/StringLearn/StringLearn.java
+++ src/cn/itcast/code/day12/StringLearn/StringLearn.java
@@ -1,4 +1,55 @@
 package cn.itcast.code.day12.StringLearn;
+/*
+字符串的本质是字符数组
+    构造方法
+    public String() 空构造
+    public String(byte[] bytes) 把字节数组转成字符串
+    public String(byte[] bytes,int offset,int length) 把字节数组的一部分转化成字符串
+    public String(char[] value) 把字符数组转成字符串
+    public String(char[] value,int offset,int count) 把字符数组的一部分转成字符串
+    public String(String original) 把字符串常量转换成字符串
 
+    字符串的方法
+        public int length()
+
+    字符串的值一旦被赋值，就不能被改变，是指值不能被改变，不是引用不能变
+
+ */
 public class StringLearn {
+    public static void main(String[] args) {
+
+        String s = new String();
+        System.out.println("s" + s + "s.length()" + s.length());
+        System.out.println("----");
+
+        byte [] bys = {97,98,99,100,101,102};
+        String s2 = new String(bys);
+        System.out.println(s2);
+        String s3 = new String(bys,2,3);
+        System.out.println(s3);
+
+        System.out.println("------");
+
+        char [] chs = {'x','y','z','爱','中','国'};
+
+        String s4 = new String(chs);
+        System.out.println(s4);
+        String s5 = new String(chs,2,4);
+        System.out.println(s5);
+
+        System.out.println("-----");
+
+        String s6 = new String("abcde");
+        System.out.println(s6);
+
+        System.out.println("--------");
+
+        String s7 = "Hello";
+        s7 += " World";
+        System.out.println(s7);
+
+
+
+
+    }
 }
--- src/cn/itcast/code/day12/day12笔记.md
+++ src/cn/itcast/code/day12/day12笔记.md
@@ -12,16 +12,16 @@ Student s = Demo.s;
 ```
 
 
-public Scanner(InputStream source):构造一个新的 Scanner，它生成的值是从指定的输入流扫描的。取自该流的字节通过底层平台的默认字符集转换成字符。
+    public Scanner(InputStream source):构造一个新的 Scanner，它生成的值是从指定的输入流扫描的。取自该流的字节通过底层平台的默认字符集转换成字符。
 
-基本格式
-    public boolean hasNextXxx():判断是否是某种类型的元素
-    public Xxx nextXxx():获取该元素
-常见方法：
-    public String nextLine():获取一个字符串类型
-    public int nextInt()：获取一个int类型
-先获取int,再获取字符串会出现问题
-    原因：将enter按照"\r\n"处理
-    如何解决：
-        A:先获取一个数值后，再创建一个新的键盘录入对象获取字符串
-        B:把所有的数据都先按照字符串获取，然后按照想要的类型进行转化
+    基本格式
+        public boolean hasNextXxx():判断是否是某种类型的元素
+        public Xxx nextXxx():获取该元素
+    常见方法：
+        public String nextLine():获取一个字符串类型
+        public int nextInt()：获取一个int类型
+    先获取int,再获取字符串会出现问题
+        原因：将enter按照"\r\n"处理
+        如何解决：
+            A:先获取一个数值后，再创建一个新的键盘录入对象获取字符串
+            B:把所有的数据都先按照字符串获取，然后按照想要的类型进行转化
